ede0ce6c90b340a64baa16220a0ada7ddbf8d2a3
Carlo Roberto <crgmzlps@gmail.com>
2021-07-26 12:12:05 -0300

    response status code

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -2,26 +2,31 @@ import {
   Body,
   Controller,
   Get,
+  HttpCode,
+  HttpStatus,
   Param,
   ParseIntPipe,
   Post,
+  Res,
 } from '@nestjs/common';
+import { Response } from 'express';
 
 @Controller('coffees')
 export class CoffeesController {
   @Get()
-  findAll() {
-    return 'This action returns all coffees';
-  }
-  @Get(':id')
-  findOne(@Param('id', ParseIntPipe) id: number) {
-    return `This action returns #${id} coffee`;
+  findAll(@Res() response: Response) {
+    response.status(200).send('This action returns all coffees');
   }
   @Get('flavors')
   flavors() {
     return 'This action returns all coffees flavors';
   }
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return `This action returns #${id} coffee`;
+  }
   @Post()
+  @HttpCode(HttpStatus.GONE) //410
   create(@Body() body) {
     return body;
   }
