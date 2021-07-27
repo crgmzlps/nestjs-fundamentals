b73e292506b0d8fc47f6542cdd6c22a9dee34774
Carlo Roberto <crgmzlps@gmail.com>
2021-07-27 16:20:51 -0300

    introducing the config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,16 +5,18 @@ import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
 import { CoffeeRatingModule } from './coffee-rating/coffee-rating.module';
 import { DatabaseModule } from './database/database.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'iluvcoffee',
+      host: process.env.DATABASE_HOST,
+      port: parseInt(process.env.DATABASE_PORT, 10),
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_DATABASE,
       autoLoadEntities: true,
       synchronize: true,
     }),
