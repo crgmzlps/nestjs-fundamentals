3d03691353a64b2197146ac550cf74111d56b53f
Carlo Roberto <crgmzlps@gmail.com>
2021-07-27 20:18:33 -0300

    fix typo var env

--- src/app.module.ts
+++ src/app.module.ts
@@ -22,7 +22,7 @@ import * as Joi from '@hapi/joi';
       port: parseInt(process.env.DATABASE_PORT, 10),
       username: process.env.DATABASE_USERNAME,
       password: process.env.DATABASE_PASSWORD,
-      database: process.env.DATABASE_DATABASE,
+      database: process.env.DATABASE_NAME,
       autoLoadEntities: true,
       synchronize: true,
     }),
