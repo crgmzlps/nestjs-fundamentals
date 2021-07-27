e4d6b8baa9cdbd9e5070abec94c00f3eb8399161
Carlo Roberto <crgmzlps@gmail.com>
2021-07-27 15:16:11 -0300

    custom providers - class providers

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -7,12 +7,22 @@ import { CoffeesService } from './coffees.service';
 import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor.entity';
 
+class ConfigService {}
+class DevelopmentConfigService {}
+class ProductionConfigService {}
 @Module({
   imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])],
   controllers: [CoffeesController],
   providers: [
     CoffeesService,
     { provide: COFFEE_BRANDS, useValue: ['buddy brew', 'nescafe'] },
+    {
+      provide: ConfigService,
+      useClass:
+        process.env.NODE_ENV === 'development'
+          ? DevelopmentConfigService
+          : ProductionConfigService,
+    },
   ],
   exports: [CoffeesService],
 })
