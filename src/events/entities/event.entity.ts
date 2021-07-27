b7af3d82aa1b48aa4fcfadeab9271624c4bf75e3
Carlo Roberto <crgmzlps@gmail.com>
2021-07-26 22:18:58 -0300

    adding indexes to entities

--- src/events/entities/event.entity.ts
+++ src/events/entities/event.entity.ts
@@ -1,5 +1,5 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
-
+import { Column, Entity, Index, PrimaryGeneratedColumn } from 'typeorm';
+@Index(['name', 'type'])
 @Entity('events')
 export class Event {
   @PrimaryGeneratedColumn()
