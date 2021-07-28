9733bfb141654877a4f6031618ec61bc17f09dfe
Carlo Roberto <crgmzlps@gmail.com>
2021-07-28 00:17:27 -0300

    using tags to group resources

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -13,11 +13,13 @@ import {
   ApiNotFoundResponse,
   ApiOkResponse,
   ApiResponse,
+  ApiTags,
 } from '@nestjs/swagger';
 import { CoffeesService } from './coffees.service';
 import { CreateCoffeeDto } from './dto/create-coffee.dto';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto';
 
+@ApiTags('coffees')
 @Controller('coffees')
 export class CoffeesController {
   constructor(private readonly coffeesService: CoffeesService) {}
