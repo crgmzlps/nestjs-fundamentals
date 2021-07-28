18310f280e9aa8808801b7b274e01cd139529d42
Carlo Roberto <crgmzlps@gmail.com>
2021-07-27 23:59:23 -0300

    introducing the swagger module

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -11,6 +12,14 @@ async function bootstrap() {
       transform: true,
     }),
   );
+  const options = new DocumentBuilder()
+    .setTitle('I Lov Coffee')
+    .setDescription('Coffe API Application')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api-docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
