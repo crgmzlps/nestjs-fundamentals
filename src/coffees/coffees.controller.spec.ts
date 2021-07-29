7b04fde9188013e0e8085bf977d1d65b55ddcb00
Carlo Roberto <crgmzlps@gmail.com>
2021-07-29 10:23:03 -0300

    fix test issue

--- src/coffees/coffees.controller.spec.ts
+++ src/coffees/coffees.controller.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CoffeesController } from './coffees.controller';
+import { CoffeesService } from './coffees.service';
 
 describe('CoffeesController', () => {
   let controller: CoffeesController;
@@ -7,6 +8,7 @@ describe('CoffeesController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [CoffeesController],
+      providers: [CoffeesService],
     }).compile();
 
     controller = module.get<CoffeesController>(CoffeesController);
