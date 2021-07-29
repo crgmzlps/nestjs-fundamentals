3e0289d9bb4468876c170e3128ae7384f3962d1a
Carlo Roberto <crgmzlps@gmail.com>
2021-07-29 11:16:19 -0300

    creating a mongoose model

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -3,14 +3,7 @@ import { Coffee } from './entities/coffee.entity';
 
 @Injectable()
 export class CoffeesService {
-  private coffees: Coffee[] = [
-    {
-      id: 1,
-      name: 'Shipwreck Roast',
-      brand: 'Buddy Brew',
-      flavors: ['Chocolate', 'Vanilla'],
-    },
-  ];
+  private coffees: Coffee[] = [];
   findAll() {
     return this.coffees;
   }
